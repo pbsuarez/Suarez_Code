84e41770d3ab72b6e649f5aee2e2db0e9a29ba48
pbsuarez <pcsuarez@up.edu.ph>
2023-08-16 15:42:04 +0800

    Added username and password inputs and a simple form validation to ensure it's not empty

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  return <div>Login Page</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    // submit logic here
+  };
+  return (
+    <>
+      <form className="login-container" onSubmit={handleSubmit(onSubmit)}>
+        <div className="login-elements">
+          <div>Username</div>
+          <input type="text" {...register("username", { required: true })} />
+          {errors.password ? (
+            <p style={{ color: "red", margin: "3px 0px" }}>
+              This is a required input
+            </p>
+          ) : null}
+        </div>
+        <div className="login-elements">
+          <div>Password</div>
+          <input
+            type="password"
+            {...register("password", { required: true })}
+          />
+          {errors.password ? (
+            <p style={{ color: "red", margin: "3px 0px" }}>
+              This is a required input
+            </p>
+          ) : null}
+        </div>
+        <button>Login</button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
