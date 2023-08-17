f73cf631a4e54cd8e209668b963daa85a5f2d23b
pbsuarez <pcsuarez@up.edu.ph>
2023-08-17 11:49:42 +0800

    final adjustments and comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/home/index" />} />
         <Route path="/account/login" element={<Login />} />
         <Route path="/home/index" element={<Dashboard />} />
       </Routes>
